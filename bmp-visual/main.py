6efd7d34f52c9ecdd789a83ff0bf968020bf401b
kutycoi123 <kutycoi123@gmail.com>
2020-06-19 21:21:01 -0700

    Added method to produce image 50% darker

--- bmp-visual/main.py
+++ bmp-visual/main.py
@@ -18,18 +18,10 @@ class Bmp24BitImage():
         self.bytesPerPixel = self.bitsPerPixel // 8
         self.bytesPerRow = ceil((self.width * self.bytesPerPixel) / 4) * 4
         self.extraBytesPerRow = self.bytesPerRow - (self.width * self.bytesPerPixel)
-        print("File size:", self.fileSize)
-        print("Width:", self.width)
-        print("Height:",self.height)
-        print("Bits per pixel:", self.bitsPerPixel)
-        print("Data offser:", self.dataOffset)
-        print("Image size:", self.imgSize)
-        print("Bytes per row:", self.bytesPerRow)
-        print("Extra byte:", self.extraBytesPerRow)
     
     def grayscale(self):
-        """ Main method to convert 24-bit bmp image to a grayscale image"""
-        grayscale = self.img[:54]
+        """ Convert 24-bit bmp image to a grayscale image"""
+        grayscale = self.img[:self.dataOffset]
         grayscale[2:6] = (0).to_bytes(4, byteorder='little') # file size
         grayscale[28:30] = (8).to_bytes(2, byteorder='little') # bit per pixel
         grayscale[46:50] = (256).to_bytes(4, byteorder='little') # color used
@@ -39,14 +31,27 @@ class Bmp24BitImage():
         
         i = self.dataOffset
         while i < len(self.img):
-            for k in range(i, i + self.width * self.bytesPerPixel, 3): # loop through all bytes on a row, except for extra bytes for padding
+            for k in range(i, i + self.width * self.bytesPerPixel, self.bytesPerPixel): # loop through all bytes on a row, except for extra bytes for padding
                 grayscale += (int(0.3 * self.img[k] + 0.59 * self.img[k+1] + 0.11 * self.img[k+2])).to_bytes(1, byteorder='little')
             if self.extraBytesPerRow > 0:
                 grayscale += (0).to_bytes(self.extraBytesPerRow, byteorder='little') # for row padding 
             i += self.bytesPerRow
         newImgSize = self.height * (self.width + self.extraBytesPerRow) # We only used 1-byte for each grayscale pixel plus some extra bytes for row padding
-        grayscale[34:38] = (newImgSize).to_bytes(4, byteorder='little')
+        grayscale[34:38] = (newImgSize).to_bytes(4, byteorder='little') # Image size
         return grayscale
+
+    def darken(self):
+        """ Darker 24-bit bmp image """
+        dark = self.img[:]
+        i = self.dataOffset
+        while i < len(self.img):
+            for k in range(i, i + self.width * self.bytesPerPixel, self.bytesPerPixel):
+                dark[k] = int(self.img[k]*0.5)
+                dark[k+1] = int(self.img[k+1]*0.5)
+                dark[k+2] = int(self.img[k+2]*0.5)
+
+            i += self.bytesPerRow
+        return dark
         
     def __readImg(self, path):
         img = bytearray()
@@ -62,9 +67,12 @@ class Bmp24BitImage():
 if __name__ == "__main__":
     path = sys.argv[1]
     img = Bmp24BitImage(path)
-    grayscale = img.grayscale()
-    with open("grayscale.bmp", 'wb') as f:
-        f.write(grayscale)
-        
+    #grayscale = img.grayscale()
+    #with open("grayscale.bmp", 'wb') as f:
+    #    f.write(grayscale)
+
+    dark = img.darken()
+    with open("dark.bmp", "wb") as f:
+        f.write(dark)
         
     
